d06c2a85af691e19518a687399fc689712440b01
Gernot Raudner <gitkraken@gernotraudner.at>
2021-08-07 19:27:11 +0200

    success indicator

---
+++ beat_manager/feedback/HitFeedbackIndicator.gd
@@ -0,0 +1,29 @@
+extends AnimatedSprite
+
+enum Status {
+	MISS,
+	TOO_FAST,
+	TOO_SLOW,
+	PERFECT
+}
+
+onready var _tween = $Tween
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _on_SongManager_beat_hit(input_delay: float) -> void:
+	var frame = Status.PERFECT
+	if abs(input_delay) > 0.15:
+		frame = Status.MISS
+	elif abs(input_delay) > 0.08:
+		if input_delay > 0:
+			frame = Status.TOO_SLOW
+		else:
+			frame = Status.TOO_FAST
+	
+	self.frame = frame
+	_tween.interpolate_property(self, "modulate", Color.white, Color.transparent, 1.0)
+	_tween.interpolate_property(self, "scale", Vector2(3, 3), Vector2(6, 6), 0.25, Tween.TRANS_BACK, Tween.EASE_OUT)
+	_tween.start()
