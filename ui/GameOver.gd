ab25798bdb34779d01cd4d480e5b3a33ecc495b0
Gernot Raudner <gitkraken@gernotraudner.at>
2021-08-08 22:49:41 +0200

    improved game over

--- ui/GameOver.gd
+++ ui/GameOver.gd
@@ -4,17 +4,20 @@ extends ColorRect
 onready var _tween = $Tween
 onready var _dance_move_count = $CenterContainer/VBoxContainer/DanceMoveCount
 onready var _reason = $CenterContainer/VBoxContainer/Reason
-
-func _update_move_count(count: int) -> void:
-	_dance_move_count.text = str(count)
+onready var _audio_player = $AudioStreamPlayer
 
 
 func show_screen(reason: String, total_count: int) -> void:
 	_reason.text = "Your heart stopped beating" if reason != "oxygen" else "Your limbs ran out of oxygen" 
-	_dance_move_count.text = str(total_count)
-	_tween.interpolate_property(self, "modulate", Color.transparent, Color.white, .5);
+	yield(get_tree().create_timer(4), "timeout")
+	_audio_player.play()
+	modulate = Color.white
 	_tween.interpolate_method(self, "_update_move_count", 0, total_count, 1);	
 	_tween.start()
+
+
+func _update_move_count(count: int) -> void:
+	_dance_move_count.text = str(count)
 
 
 func _on_PlayAgainButton_button_up() -> void:
