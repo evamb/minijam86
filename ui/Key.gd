d4dfeaaecd51d28de69e7c18dbfff4cbc7f146e2
evamb <evabrucker@aon.at>
2021-08-09 00:13:38 +0200

    minor changes

--- ui/Key.gd
+++ ui/Key.gd
@@ -7,7 +7,7 @@ onready var _tween = $Tween
 onready var _label = $Label
 
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed(action):
-		_tween.interpolate_property(self, "modulate", Color.yellow, Color.white, 0.5)
-		_tween.start()
+#func _process(delta: float) -> void:
+#	if Input.is_action_just_pressed(action):
+#		_tween.interpolate_property(self, "modulate", Color.yellow, Color.white, 0.5)
+#		_tween.start()
