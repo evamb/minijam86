5b5f582c0312acad645ba45defa6e7125a65a95f
Gernot Raudner <gitkraken@gernotraudner.at>
2021-08-08 17:07:07 +0200

    dance moves

--- Globals.gd
+++ Globals.gd
@@ -2,3 +2,12 @@ extends Node
 
 const MISS_THRESHOLD = 0.15
 const PERFECT_THRESHOLD = 0.08
+
+const DANCE_MOVES = [
+	{
+		"arm_left": 4,
+		"arm_right": 4,
+		"leg_left": 3,
+		"leg_right": 3,
+	}
+]
--- oxygen_manager/OxygenManager.gd
+++ oxygen_manager/OxygenManager.gd
@@ -3,6 +3,7 @@ extends Node2D
 signal oxygen_updated
 signal limb_died
 signal died
+signal beat_clock
 
 var _limb_oxygen_count = {
 	"arm_left": 2,
@@ -13,6 +14,8 @@ var _limb_oxygen_count = {
 var _limb_index = 0
 var _has_started = false
 var _dead_limbs = []
+var _dance_prepared = false
+var _clock_count = 0
 
 onready var _limb_locations = {
 	"arm_left": $ArmLeft,
@@ -33,10 +36,15 @@ onready var _dead_limb_sprites = {
 	"leg_left": $LegLeft/Broken,
 }
 onready var _tween = $Tween
+onready var _dance_move_label = $DanceMoveLabel
 onready var Oxygen = preload("res://oxygen_manager/Oxygen.tscn")
 
 
 func _ready() -> void:
+	randomize()
+	_prepare_dance()
+	# this is needed for the first note
+	_clock_count += 1
 	_update_labels(_limb_oxygen_count)
 # warning-ignore:return_value_discarded
 	connect("oxygen_updated", self, "_update_labels")
@@ -65,7 +73,7 @@ func _spawn_oxygen(input_action) -> void:
 	_tween.start()
 	yield(get_tree().create_timer(0.5), "timeout")
 	oxygen.queue_free()
-	_limb_oxygen_count[action] += 1
+	_limb_oxygen_count[action] -= 1
 	emit_signal("oxygen_updated", _limb_oxygen_count)
 
 
@@ -74,29 +82,52 @@ func _update_labels(limb_oxygen_count) -> void:
 	var dying_limbs = []
 	for limb in keys:
 		if limb_oxygen_count[limb] <= 0:
-			dying_limbs.append(limb)
-			emit_signal("limb_died", limb)
-			_tween.interpolate_property(_dead_limb_sprites[limb], "modulate", Color.transparent, Color.white, 1)
-			_tween.start()
-			_limb_labels[limb].text = "X"
+			_limb_labels[limb].text = "OK!"
 		else:
 			_limb_labels[limb].text = str(limb_oxygen_count[limb])
 			
-	for dead_limb in dying_limbs:
-		_limb_labels.erase(dead_limb)
-		_dead_limbs.append(dead_limb)
-	if _dead_limbs.size() == 4:
-		emit_signal("died")
-		
+
 
 func _on_SongManager_started(_bpm: float) -> void:
 	_has_started = true
 
 
+func _prepare_dance() -> void:
+	var requirements = Globals.DANCE_MOVES[randi() % Globals.DANCE_MOVES.size()]
+	_limb_oxygen_count = requirements.duplicate()
+	var rest_oxygen = 0
+	for dead in _dead_limbs:
+		rest_oxygen += _limb_oxygen_count[dead]
+	var living_limbs = _limb_labels.keys()
+	while rest_oxygen > 0:
+		rest_oxygen -= 1
+		_limb_oxygen_count[living_limbs[randi() % living_limbs.size()]] += 1
+	
+	emit_signal("oxygen_updated", _limb_oxygen_count)
+	_clock_count = 7
+
+
+func _check_limbs() -> void:
+	var limbs = _limb_labels.keys()
+	for limb in limbs:
+		if _limb_oxygen_count[limb] > 0:
+			_dead_limbs.append(limb)
+			emit_signal("limb_died", limb)
+			_tween.interpolate_property(_dead_limb_sprites[limb], "modulate", Color.transparent, Color.white, 1)
+			_tween.start()
+			_limb_labels[limb].text = "X"
+			_limb_labels.erase(limb)
+	if _dead_limbs.size() == 4:
+		emit_signal("died")
+	else:
+		_prepare_dance()
+
+
 func _on_SongManager_beat_clock() -> void:
+	_clock_count -= 1
+	emit_signal("beat_clock")
+	if _clock_count < 0:
+		_check_limbs()
 	if not _has_started or _limb_labels.size() == 0:
 		return
-	_limb_index += 1
-	var limbs = _limb_labels.keys()
-	_limb_oxygen_count[limbs[_limb_index % limbs.size()]] -= 1
-	emit_signal("oxygen_updated", _limb_oxygen_count)
+	_dance_move_label.text = str(_clock_count)
